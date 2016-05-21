04e1b8d0299e4c351282e9d98ef2e010e2eea688
ChunkLightTuna <chris@oeleri.ch>
2016-05-21 09:50:06 -0400

    dummy start button

--- app/src/main/java/edu/unh/cs/android/dopesafe/MainActivity.java
+++ app/src/main/java/edu/unh/cs/android/dopesafe/MainActivity.java
@@ -1,19 +1,21 @@
 package edu.unh.cs.android.dopesafe;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
     implements NavigationView.OnNavigationItemSelectedListener {
+
+  private static final String TAG = "MainActivity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +24,20 @@ public class MainActivity extends AppCompatActivity
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
+    Button startButton = (Button) findViewById(R.id.start_button);
+
+    if (startButton != null) {
+      startButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          startTimer();
+        }
+      });
+    }
+  }
+
+  private void startTimer() {
+    Log.d(TAG, "startTimer() called with: " + "");
   }
 
   @Override
