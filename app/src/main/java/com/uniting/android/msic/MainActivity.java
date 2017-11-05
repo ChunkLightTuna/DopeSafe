f40e8c6f1a6503c5b5eab7c1176e9ed8be22fe38
ChunkLightTuna <chris@oeleri.ch>
2017-11-05 14:28:50 -0500

    merge 1&2

--- app/src/main/java/com/uniting/android/msic/MainActivity.java
+++ app/src/main/java/com/uniting/android/msic/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.os.Bundle;
@@ -13,12 +14,18 @@ import android.os.SystemClock;
 import android.os.Vibrator;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -26,21 +33,17 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 
-/**
- * Created by Chris Oelerich on 5/20/16.
- * <p/>
- * Main
- */
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements
+    NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
-    private static final int PERMISSIONS_REQUEST_SEND_SMS = 15423;
+    private final int PERMISSIONS_REQUEST_ALL_NECESSARY = 15423;
+
 
     private UserPrefs prefs;
 
     private TextView time;
     private Button startButton;
-    private Switch stopButton;//  SeekBar stopButton;
+    private Switch stopButton;
     private ProgressBar progressCircle;
 
     private long startTime = 0L;
@@ -51,21 +54,22 @@ public class MainActivity extends AppCompatActivity {
     private int seconds = 0;
     private int minutes = 0;
     private boolean alarm = false;
+    private boolean havePermissionForFineLocation = false;
+    private boolean havePermissionForCoarseLocation = false;
 
     private Handler handler = new Handler();
     private Runnable timer;
 
     private Ringtone ringtone;
     private Vibrator vibrator;
+    private LocationService locationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate() called with: " + "savedInstanceState = [" + savedInstanceState + "]");
-
-        setContentView(com.uniting.android.msic.R.layout.activity_main);
-
-        startButton = findViewById(R.id.start_button);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         prefs = UserPrefs.getInstance();
         getSharedPrefs(getPreferences(Context.MODE_PRIVATE));
@@ -89,19 +93,15 @@ public class MainActivity extends AppCompatActivity {
         time = findViewById(R.id.time);
         updateTime();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        if (navigationView != null)
-            navigationView.setNavigationItemSelectedListener(new SettingsMenu(this, prefs));
-
+        startButton = findViewById(R.id.start_button);
         if (startButton != null) {
-            startButton.setOnClickListener(v -> startTimer());
+            startButton.setOnClickListener(v -> confirmInitializeOfSession());
         }
 
         stopButton = findViewById(R.id.stop_button);
         if (stopButton != null) {
             stopButton.setVisibility(View.INVISIBLE);
             stopButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-
                 Log.d(TAG, "onCheckedChanged() called with: " + "buttonView = [" + buttonView + "], isChecked = [" + isChecked + "]");
                 if (isChecked) {
                     // The toggle is enabled
@@ -136,16 +136,44 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new SettingsMenu(this, prefs));
+
+
+
         getPermissions();
+        locationService = new LocationService(this);
     }
 
     private void getPermissions() {
+        String[] permissions = new String[]{Manifest.permission.SEND_SMS,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if (!allPermissionsAreGranted())
+            ActivityCompat.requestPermissions(this, permissions, PERMISSIONS_REQUEST_ALL_NECESSARY);
+    }
+
+    private boolean allPermissionsAreGranted() {
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.SEND_SMS},
-                    PERMISSIONS_REQUEST_SEND_SMS);
+            Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return false;
         }
+        return true;
     }
 
     @Override
@@ -155,9 +183,112 @@ public class MainActivity extends AppCompatActivity {
         setSharedPrefs();
     }
 
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ALL_NECESSARY:
+                if (!permissionsGranted(grantResults))
+                    showPermissionDialog();
+                return;
+        }
+    }
+
+    public void showPermissionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.permissions_dialog_message)
+            .setTitle(R.string.permissions_dialog_title)
+            .setPositiveButton(R.string.permissions_dialog_positive_button_text,
+                (dialogInterface, i) -> {
+                    //
+                })
+            .setOnDismissListener(dialogInterface -> {
+                getPermissions();
+            })
+            .create()
+            .show();
+    }
+
+    private boolean permissionsGranted(int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        }
+
+        if (grantResults.length == 0)
+            return false;
+        else
+            for (int result : grantResults)
+                if (result != PackageManager.PERMISSION_GRANTED)
+                    return false;
+
+        return true;
+    }
+
+    private void tryToInitLocationService() {
+        if (this.havePermissionForFineLocation && this.havePermissionForCoarseLocation)
+            this.locationService = new LocationService(this);
+    }
+
     public void updateTime() {
         time.setText(String.format(getString(com.uniting.android.msic.R.string.display_time), prefs.getTime(), 0));
     }
+
 
     private void getSharedPrefs(SharedPreferences sharedPref) {
         Log.d(TAG, "getSharedPrefs() called with: " + "sharedPref = [" + sharedPref + "]");
@@ -203,7 +334,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void startTimer() {
         Log.d(TAG, "startTimer() called with: " + "");
-
         if (t) {
 //timer will start
             startButton.setVisibility(View.INVISIBLE);
@@ -218,10 +348,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void sendSMS(String number, String message) {
+    private void confirmInitializeOfSession() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.init_dialog_message)
+            .setTitle(R.string.init_dialog_title)
+            .setPositiveButton(R.string.init_dialog_positive_button_text, (dialogInterface, i) -> startTimer())
+            .setNegativeButton(R.string.init_dialog_negative_button_text, (dialogInterface, i) -> {
+                //
+            })
+            .create()
+            .show();
+    }
 
+
+    public void sendSMS(String number, String message, Location location) {
         try {
             SmsManager smsManager = SmsManager.getDefault();
+            message += "\nLocation: " + getGoogleMapsUrl(location);
             smsManager.sendTextMessage(number, null, message, null, null);
             Log.d(TAG, "sendSMS() called with: " + "number = [" + number + "], message = [" + message + "]");
         } catch (Exception e) {
@@ -229,24 +372,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_SEND_SMS: {
-                if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "onRequestPermissionsResult: permissions not granted!");
-                    View contentView = findViewById(android.R.id.content);
-                    if (contentView != null) {
-                        final Snackbar snackbar = Snackbar.make(contentView, "App will not work if you don't grant it SMS permissions", Snackbar.LENGTH_INDEFINITE);
-
-                        snackbar.getView().setOnClickListener(v -> {
-                            snackbar.dismiss();
-                            getPermissions();
-                        });
-                        snackbar.show();
-                    }
-                }
-            }
+    public String getGoogleMapsUrl(Location location) {
+        //TODO handle location issues better
+        try {
+            StringBuilder url = new StringBuilder();
+            url.append("http://maps.google.com?q=");
+            url.append(location.getLatitude());
+            url.append(",");
+            url.append(location.getLongitude());
+            return url.toString();
+        } catch (NullPointerException e) {
+            Log.e(TAG, "unable to get location string(Probably because emulator is being used and location was not sent.)", e);
+            return "Location unavailable";
         }
     }
 
@@ -266,7 +403,7 @@ public class MainActivity extends AppCompatActivity {
 
                 //times up
                 if (minutes == prefs.getTime()) {
-                    sendSMS(prefs.getPhone(), prefs.getMsg());
+                    sendSMS(prefs.getPhone(), prefs.getMsg(), locationService.getLocation());
                     time.setText(String.format(getString(com.uniting.android.msic.R.string.display_time), 0, 0));
                     time.setTextColor(Color.RED);
                 } else {
