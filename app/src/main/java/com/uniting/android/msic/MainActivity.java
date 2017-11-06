eba54fc0c0653d8f60b6ef2e3426dc11f3e67ab1
ChunkLightTuna <chris@oeleri.ch>
2017-11-05 21:03:57 -0500

    FR005, FR007 and FR008

--- app/src/main/java/com/uniting/android/msic/MainActivity.java
+++ app/src/main/java/com/uniting/android/msic/MainActivity.java
@@ -135,7 +135,6 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
             this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -193,17 +192,24 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Handle action bar item clicks here. The action bar will
+     * automatically handle clicks on the Home/Up button, so long
+     * as you specify a parent activity in AndroidManifest.xml.
+
+     * @param item MenuItem
+     * @return resource has been consumed
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        /*
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
+//        noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        */
 
         return super.onOptionsItemSelected(item);
     }
