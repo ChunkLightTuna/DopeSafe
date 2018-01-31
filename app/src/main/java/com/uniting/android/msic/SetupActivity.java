7e25187a3e5f5a579105537736191738a518d646
Jeep Peck <jeep.peck@gmail.com>
2018-01-31 16:27:20 -0700

    remove setup activity from back stack

--- app/src/main/java/com/uniting/android/msic/SetupActivity.java
+++ app/src/main/java/com/uniting/android/msic/SetupActivity.java
@@ -59,5 +59,7 @@ public class SetupActivity extends AppCompatActivity implements StepperLayout.St
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+        finish();
     }
+
 }
